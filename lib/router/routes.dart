360cad186ccb7f47d361095f83daf20f5c6de0be
Paramxt <136378697+Paramxt@users.noreply.github.com>
2024-03-28 15:44:56 +0700

    Add files via upload

--- lib/router/routes.dart
+++ lib/router/routes.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter_workshop/router/routes.gr.dart';
+import 'package:flutter_workshop/screen/Mainhome.dart';
 
 @AutoRouterConfig()
 class AppRouter extends $AppRouter {
@@ -11,12 +12,28 @@ class AppRouter extends $AppRouter {
             initial: true,
             transitionsBuilder: TransitionsBuilders.slideLeft),
         CustomRoute(
-            path: '/login',
+            path: '/signin',
             page: LoginRoute.page,
             transitionsBuilder: TransitionsBuilders.slideTop),
         CustomRoute(
             path: '/example',
             page: ExampleRoute.page,
+            transitionsBuilder: TransitionsBuilders.slideLeft),
+        CustomRoute(
+            path: '/signup',
+            page: SignUpRoute.page,
+            transitionsBuilder: TransitionsBuilders.slideLeft),
+        CustomRoute(
+            path: '/page0',
+            page: MainhomeRoute.page,
+            transitionsBuilder: TransitionsBuilders.slideLeft),
+        CustomRoute(
+            path: '/page1',
+            page: SettingRoute.page,
+            transitionsBuilder: TransitionsBuilders.slideLeft),
+        CustomRoute(
+            path: '/eiei',
+            page: EieiRoute.page,
             transitionsBuilder: TransitionsBuilders.slideLeft)
       ];
 }
--- lib/router/routes.gr.dart
+++ lib/router/routes.gr.dart
@@ -8,32 +8,67 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i4;
+import 'package:auto_route/auto_route.dart' as _i9;
+import 'package:flutter_workshop/screen/Adddevice.dart' as _i6;
+import 'package:flutter_workshop/screen/eiei.dart' as _i8;
 import 'package:flutter_workshop/screen/example.dart' as _i1;
 import 'package:flutter_workshop/screen/home.dart' as _i2;
-import 'package:flutter_workshop/screen/login.dart' as _i3;
+import 'package:flutter_workshop/screen/Mainhome.dart' as _i4;
+import 'package:flutter_workshop/screen/Sensor.dart' as _i5;
+import 'package:flutter_workshop/screen/SignIn.dart' as _i3;
+import 'package:flutter_workshop/screen/SignUp.dart' as _i7;
 
-abstract class $AppRouter extends _i4.RootStackRouter {
+abstract class $AppRouter extends _i9.RootStackRouter {
   $AppRouter({super.navigatorKey});
 
   @override
-  final Map<String, _i4.PageFactory> pagesMap = {
+  final Map<String, _i9.PageFactory> pagesMap = {
     ExampleRoute.name: (routeData) {
-      return _i4.AutoRoutePage<dynamic>(
+      return _i9.AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const _i1.ExampleRoute(),
       );
     },
     HomeRoute.name: (routeData) {
-      return _i4.AutoRoutePage<dynamic>(
+      return _i9.AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const _i2.HomeRoute(),
       );
     },
     LoginRoute.name: (routeData) {
-      return _i4.AutoRoutePage<dynamic>(
+      return _i9.AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const _i3.LoginPage(),
+      );
+    },
+    MainhomeRoute.name: (routeData) {
+      return _i9.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i4.MainhomePage(),
+      );
+    },
+    Route2Route.name: (routeData) {
+      return _i9.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i5.Page2Page(),
+      );
+    },
+    SettingRoute.name: (routeData) {
+      return _i9.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i6.SettingPage(),
+      );
+    },
+    SignUpRoute.name: (routeData) {
+      return _i9.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i7.SignUpPage(),
+      );
+    },
+    EieiRoute.name: (routeData) {
+      return _i9.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i8.eieiPage(),
       );
     },
   };
@@ -41,8 +76,8 @@ abstract class $AppRouter extends _i4.RootStackRouter {
 
 /// generated route for
 /// [_i1.ExampleRoute]
-class ExampleRoute extends _i4.PageRouteInfo<void> {
-  const ExampleRoute({List<_i4.PageRouteInfo>? children})
+class ExampleRoute extends _i9.PageRouteInfo<void> {
+  const ExampleRoute({List<_i9.PageRouteInfo>? children})
       : super(
           ExampleRoute.name,
           initialChildren: children,
@@ -50,13 +85,13 @@ class ExampleRoute extends _i4.PageRouteInfo<void> {
 
   static const String name = 'ExampleRoute';
 
-  static const _i4.PageInfo<void> page = _i4.PageInfo<void>(name);
+  static const _i9.PageInfo<void> page = _i9.PageInfo<void>(name);
 }
 
 /// generated route for
 /// [_i2.HomeRoute]
-class HomeRoute extends _i4.PageRouteInfo<void> {
-  const HomeRoute({List<_i4.PageRouteInfo>? children})
+class HomeRoute extends _i9.PageRouteInfo<void> {
+  const HomeRoute({List<_i9.PageRouteInfo>? children})
       : super(
           HomeRoute.name,
           initialChildren: children,
@@ -64,13 +99,13 @@ class HomeRoute extends _i4.PageRouteInfo<void> {
 
   static const String name = 'HomeRoute';
 
-  static const _i4.PageInfo<void> page = _i4.PageInfo<void>(name);
+  static const _i9.PageInfo<void> page = _i9.PageInfo<void>(name);
 }
 
 /// generated route for
 /// [_i3.LoginPage]
-class LoginRoute extends _i4.PageRouteInfo<void> {
-  const LoginRoute({List<_i4.PageRouteInfo>? children})
+class LoginRoute extends _i9.PageRouteInfo<void> {
+  const LoginRoute({List<_i9.PageRouteInfo>? children})
       : super(
           LoginRoute.name,
           initialChildren: children,
@@ -78,5 +113,75 @@ class LoginRoute extends _i4.PageRouteInfo<void> {
 
   static const String name = 'LoginRoute';
 
-  static const _i4.PageInfo<void> page = _i4.PageInfo<void>(name);
+  static const _i9.PageInfo<void> page = _i9.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i4.MainhomePage]
+class MainhomeRoute extends _i9.PageRouteInfo<void> {
+  const MainhomeRoute({List<_i9.PageRouteInfo>? children})
+      : super(
+          MainhomeRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'MainhomeRoute';
+
+  static const _i9.PageInfo<void> page = _i9.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i5.Page2Page]
+class Route2Route extends _i9.PageRouteInfo<void> {
+  const Route2Route({List<_i9.PageRouteInfo>? children})
+      : super(
+          Route2Route.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'Route2Route';
+
+  static const _i9.PageInfo<void> page = _i9.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i6.SettingPage]
+class SettingRoute extends _i9.PageRouteInfo<void> {
+  const SettingRoute({List<_i9.PageRouteInfo>? children})
+      : super(
+          SettingRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'SettingRoute';
+
+  static const _i9.PageInfo<void> page = _i9.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i7.SignUpPage]
+class SignUpRoute extends _i9.PageRouteInfo<void> {
+  const SignUpRoute({List<_i9.PageRouteInfo>? children})
+      : super(
+          SignUpRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'SignUpRoute';
+
+  static const _i9.PageInfo<void> page = _i9.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i8.eieiPage]
+class EieiRoute extends _i9.PageRouteInfo<void> {
+  const EieiRoute({List<_i9.PageRouteInfo>? children})
+      : super(
+          EieiRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'EieiRoute';
+
+  static const _i9.PageInfo<void> page = _i9.PageInfo<void>(name);
 }
