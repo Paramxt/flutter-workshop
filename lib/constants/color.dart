a126d851ab2f57deaa2f6fcee3cd4d5aecc5999e
Paramxt <136378697+Paramxt@users.noreply.github.com>
2024-03-28 16:02:09 +0700

    Add files via upload

---
+++ lib/constants/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final Color BottomColor = const Color.fromARGB(255, 78, 181, 145);
+final Color FontColor = const Color.fromARGB(255, 14, 13, 13);
+final Color BoxColor = const Color.fromARGB(255, 235, 235, 235);
+final Color GrayColor = const Color.fromARGB(255, 217, 217, 217);
+final Color SwitchColor = const Color.fromARGB(255, 14, 13, 13);
